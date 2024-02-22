0a958f8fa204079e55df4334c3eea2834c45b6c2
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-02-23 03:05:30 +0530

    Initial commit with google login and sign In features

---
+++ pages/login.js
@@ -0,0 +1,23 @@
+
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Component() {
+    const { data: session } = useSession();
+    console.log(session);
+    console.log("hey");
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn("google")}>Sign in</button>
+    </>
+  );
+}
